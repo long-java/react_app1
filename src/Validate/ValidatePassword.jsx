f508e1674858fa309ae7567f96a1b081ae7fb33c
Long Phạm Tiến <phamtienlong0309@gmail.com>
2024-12-10 22:36:40 +0700

    add validate yup, yupResolver, test MUI Select

--- src/Validate/ValidatePassword.jsx
+++ src/Validate/ValidatePassword.jsx
@@ -4,17 +4,17 @@ import { Controller } from "react-hook-form";
 import { FormControl, TextField } from "@mui/material";
 import PropTypes from 'prop-types'; // Import PropTypes
 
-const ValidateUserName = ({ control, id }) => {
+const ValidatePassword = ({ control, id }) => {
   return (
     <Controller
-      name="userName" // Tên trường dữ liệu
+      name="password" // Tên trường dữ liệu
       control={control} // Truyền control từ form
       defaultValue="" // Giá trị mặc định
       render={({ field, fieldState }) => (
         <FormControl fullWidth>
           <TextField
             {...field} // Truyền props từ field vào TextField
-            label="UserName"
+            label="Password"
             variant="outlined"
             fullWidth
             error={!!fieldState?.error} // Nếu có lỗi, hiển thị thông báo
@@ -28,9 +28,9 @@ const ValidateUserName = ({ control, id }) => {
 };
 
 // Định nghĩa propTypes
-ValidateUserName.propTypes = {
+ValidatePassword.propTypes = {
   control: PropTypes.object.isRequired, // Kiểu dữ liệu của prop `control` là object và bắt buộc phải có
   id: PropTypes.string.isRequired, // Kiểu dữ liệu của prop `id` là string và bắt buộc phải có
 };
 
-export default ValidateUserName;
+export default ValidatePassword;
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Button, TextField, Stack } from '@mui/material';
+import { Button, TextField, Stack, FormControl, Select, InputLabel, MenuItem } from '@mui/material';
 import ValidateUserName from "../Validate/ValidateUserName";
+import ValidatePassword from "../Validate/ValidatePassword";
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 function App() {
-  // Khởi tạo react-hook-form và lấy control
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    mode: 'onSubmit' // Đảm bảo chỉ validate khi submit
-  });
-
   // Hàm xử lý khi form được submit
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  // data cho select Level
+  const levels = ['Dev1', 'Dev2', 'Dev3', 'Dev4', 'Dev5'];
+  const [selectedLevel, setSelectedLevel] = useState("Dev1"); // Giá trị mặc định
+  const handleChangeSelectLevel = (e) => {
+    setSelectedLevel(e.target.value);
+  }
+
+  const REGEX_EMAIL = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  // tạo schema cho Validation
+  const validateSchema = Yup.object({
+    userName: Yup.string().required('UserName is required from Yup').min(6, 'UserName must be min 6 from Yup'),
+    email: Yup.string().required('Email is required from Yup').matches(REGEX_EMAIL, 'Not match REGEX_EMAIL.'),
+    password: Yup.string().required('Password is required from Yup').min(6, 'Password must be min 6 from Yup'),
+    level: Yup.string().required('Level is required from Yup').oneOf(levels, 'Invalid oneOf from Yub')
+  });
+
+  // Khởi tạo react-hook-form và lấy control
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validateSchema),
+    mode: 'onSubmit' // Đảm bảo chỉ validate khi submit
+  });
 
   return (
     <Stack spacing={2} width="300px" margin="auto" padding={2}>
@@ -29,18 +49,41 @@ function App() {
           name="email"
           control={control}
           defaultValue=""
-          rules={{ required: 'Email is required' }}
           render={({ field }) => (
-            <TextField
-              {...field}
-              label="Email"
-              variant="outlined"
-              fullWidth
-              error={!!errors.email}
-              helperText={errors.email ? errors.email.message : ''}
-            />
+            <FormControl fullWidth>
+              <TextField
+                {...field}
+                label="Email"
+                variant="outlined"
+                fullWidth
+                error={!!errors.email}
+                helperText={errors.email ? errors.email.message : ''}
+              />
+            </FormControl>
           )}
         />
+
+        {/* Validation from Yup */}
+        <ValidatePassword
+          control={control}
+          id="test"
+        />
+
+        {/* test Select */}
+        <FormControl fullWidth>
+          <Select
+            name='level'
+            value={selectedLevel} // Giá trị được điều khiển
+            onChange={handleChangeSelectLevel} // Xử lý sự kiện thay đổi
+            fullWidth
+          >
+            {levels.map((level) => (
+              <MenuItem key={level} value={level}>
+                {level}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
         {/* Submit Button */}
         <Button type="submit" variant="contained">Submit</Button>
